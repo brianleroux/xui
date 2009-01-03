5b18b17302c61bf9700d50b6b88f25807f5f3eca
Brian LeRoux <brian.leroux@westcoastlogic.com>
2009-01-02 16:09:03 -0800

    new repo structure and build ideas being tested

--- Rakefile
+++ Rakefile
@@ -1,7 +1,23 @@
+require 'erb'
 
-default = [:minify]
-
-desc 'builds the xui source'
-task :build
-
+task :default do
+  write
 end 
+
+# - helpers
+
+LIBPATH = File.expand_path(File.dirname(__FILE__)) + File::SEPARATOR
+
+# writes out an uncompiled version of xui
+def write
+  path = "#{ LIBPATH }src#{ File::SEPARATOR }js#{ File::SEPARATOR }xui.js"
+  template = ERB.new(open(path).read)
+  puts template.result
+end
+
+# used in precompiled xui.js
+def import(fname)
+  path = "#{ LIBPATH }src#{ File::SEPARATOR }js#{ File::SEPARATOR }lib#{ File::SEPARATOR }#{ fname }.js"
+  erb = File.open(path) { |fp| ERB.new(fp.read) }
+  erb.run
+end
---
+++ src/js/lib/event.js
@@ -0,0 +1,60 @@
+//
+//  ----- Event methods  -----
+//
+//  dependencies
+//  - xui-core
+//
+var Event = {
+	eventfunctions : [],
+	click: function(fn) { return this.on('click',fn); },
+	dblclick: function(fn) { return this.on('dblclick',fn); },
+	load: function(fn) { return this.on('load',fn); },
+	on: function(type, fn) {
+	    var listen = function(el) {
+	        if (window.addEventListener) {
+	            el.addEventListener(type, fn, false);
+	        }
+	    };
+	    this.each(function(el) {
+	        listen(el);
+	    });
+	    return this;
+	},
+	subscribe: function(fn) {
+	    this.eventfunctions.push(fn);
+	},
+	unsubscribe: function(fn) {
+	    this.eventfunctions = this.eventfunctions.filter (
+	        function(el) {
+	            if ( el !== fn ) {
+	                return el;
+	            }
+	        }
+	    );
+	},
+	fire: function(o, thisObj) {
+	    var scope = thisObj || window;
+	    this.eventfunctions.forEach(
+	        function(el) {
+	            el.call(scope, o);
+	        }
+	    );
+	},
+	
+    stop: function(e) {
+        if(window.event && window.event.returnValue)
+            window.event.returnValue = false;
+
+        if(e && e.preventDefault)
+            e.preventDefault();
+    },
+
+    bind: function(fn,context) {
+    	var args = [];
+        for (var i = 2; i < arguments.length; i++) {
+        	args[i - 2] = arguments[i];
+        }
+        fn.apply(context,args);       
+        return this;
+     }
+}
