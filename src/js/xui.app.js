fb41c4277278660b654dc1cee076cf09f6e65beb
Brian LeRoux <brian.leroux@westcoastlogic.com>
2009-01-04 15:07:46 -0800

    quick fix for the example before documenting

--- src/js/xui.app.js
+++ src/js/xui.app.js
@@ -1,5 +1,8 @@
 x$(window).load(function(){
 	
+	// load the default content by default 
+	x$('#content').xhr('_index.html')
+	
 	// relative links loaded via ajax and animated in (with a left to right swipe by default)
 	x$('div#stage ul.nav li a').click(function(e) {
 		var url = this.href;
