d380b1e6a464793f6da5adab980513fbc0b967c0
Brian LeRoux <brian.leroux@westcoastlogic.com>
2009-01-06 10:13:00 -0800

    hacking on an idea for mvc xui-app with rob

--- src/js/xui.app.js
+++ src/js/xui.app.js
@@ -16,3 +16,30 @@ x$(window).load(function(){
 		
 	});
 });	
+
+
+
+
+x$.app('my special app', function(){
+	
+	// private methods
+	var _history = [];
+	var _viewCache = [];
+
+	// controllers methods that point to views 
+	// views may or may not contain headers
+	return {
+		layout:'index.html',
+		default:'_index.html',
+		before:function(){},
+		after:function(){},
+		
+		'about':function(options){ 
+			alert('page onload callback') 
+			$('#content').xhrjson('/get_json_from_sinatra', {partial:'_foo', map:{head:'.header', body:'.body'}})
+		},
+		
+		'contribute':function(){ alert('page onload callback') },
+		'foo/bar/exmaple.html':function(){ alert('page onload callback') },
+	}
+});
