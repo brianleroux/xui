0968b02abb941ad359e61a15aefa158d4abd08f5
Rob Ellis <kazoomer@gmail.com>
2009-01-07 19:51:39 -0800

    XAPP Init

--- src/js/xui.app.js
+++ src/js/xui.app.js
@@ -18,32 +18,40 @@
 // });	
 
 x$.app = function(title,controller) {
-	console.log(title);
-	controller.apply(this);
-}
 
-
-x$.app('my special app', function(){
-  // private methods
 	var _history = [];
 	var _viewCache = [];
 
-	// controllers methods that point to views 
-	// views may or may not contain headers
-	return {
+	var currentRequest = null
+
+	for(var action in controller) {
+		if (action == 'before')
+			controller[action]();
+	}
+
+
+	for(var action in controller) {
+		if (action == 'after')
+			controller[action]();
+	}
+	
+}
+
+
+
+
+x$.app('my special app', {
+
 	 	'layout':'index.html',
 	 	'default':'_index.html',
-	 	'before':function(){},
-	 	'after':function(){},
+	 	'before':function(){ alert("Before");},
+	 	'after':function(){alert("After");},
 	 	
 	 	'about':function(options){ 
 	 		alert('page onload callback') 
 	 		$('#content').xhrjson('/get_json_from_sinatra', {partial:'_foo', map:{head:'.header', body:'.body'}})
 	 	}
-	// 	
-	// 	'contribute':function(){ alert('page onload callback') },
-	// 	'foo/bar/exmaple.html':function(){ alert('page onload callback') },
-	}
+
 });
 
 
