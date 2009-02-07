df01734ba3fef295b08a463f463a763d6be340eb
Brian LeRoux <brian.leroux@westcoastlogic.com>
2009-02-07 14:20:08 -0800

    mostly working fx example. old bugs popping up for some reason. =/

--- example/fx/fx.js
+++ example/fx/fx.js
@@ -1,7 +1,19 @@
 var initFxExample = function() {
 	x$('#stage').html(this.responseText);
 	
-	x$('.box').click(function(e){
-		x$('.box').tween({rotate:{Z:90}});
+	x$('#fx-button').click(function(e){
+		x$('#fx').tween([
+			{
+				background:'red',
+				right:'0px',
+				duration:2.5
+			},
+			{
+				background:'blue',
+				bottom:'0px',
+				duration:2.5
+			}
+		]);
 	});
+/// ---	
 }
