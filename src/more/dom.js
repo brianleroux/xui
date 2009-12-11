a439da3f4f1144701844d7007316812a8c79fcd0
Remy Sharp <remy@remysharp.com>
2009-12-11 01:53:15 +0000

    Squeeeeeeeze :)

--- src/more/dom.js
+++ src/more/dom.js
@@ -1,24 +1,3 @@
-xui.extend({
-	inner: function(html) {
-	    return this.html('inner', html);
-	},
-	outer: function(html) {
-	    return this.html('outer', html);
-	},
-	top: function(html) {
-	    return this.html('top', html);
-	},
-	bottom: function(html) {
-	    return this.html('bottom', html);
-	},
-	remove: function() {
-	    return this.html('remove');
-	},
-	before: function(html) {
-	    return this.html('before', html);
-	},
-	after: function(html) {
-	    return this.html('after', html);
-	}	
-// --	
+"inner outer top bottom remove before after".split(' ').forEach(function (method) {
+  xui.extend({method: function (html) { return this.html(method, html); }});
 });
