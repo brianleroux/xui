d26ae856f755c69d35bbfba1e0be0594a78b3de1
Remy Sharp <remy@leftlogic.com>
2010-03-31 16:22:19 +0100

    Removed anonymous function wrapper - not required as whole library is wrapped. Also has effects working, and converting XUI tween options to Emile. Also added touch flag so we know which type of events to bind.

--- src/core/fx.js
+++ src/core/fx.js
@@ -35,15 +35,43 @@ xui.extend({
 	 *
 	 * 	x$('#box').tween({ left:100px, backgroundColor:'blue' });
 	 * 	x$('#box').tween({ left:100px, backgroundColor:'blue' }, function() { alert('done!'); });
-	 * 	x$('#box').tween([{ left:100px, backgroundColor:'green', duration:.2 }, { right:'100px' }]);
+	 * 	x$('#box').tween([{ left:100px, backgroundColor:'green', duration:.2 }, { right:'100px' }]); // WTF? is this a sequence?
 	 * 	x$('#box').tween({ left:100px}).tween({ left:'100px' });
 	 * 
 	 */
-	tween: function( options, callback ) {
-		// TODO make xui into emile options
+	tween: function( props, options, callback ) {
 		// TODO make queue
-		return this.each(function(e){		
-			emile(e, options, callback);
+		
+		if (typeof options == 'function') {
+		  callback = options;
+		  options = {};
+		} else if (typeof options == 'number') {
+		  options = { duration: options };
+		}
+		
+		// Here be XUI to emile options
+		
+		if (typeof callback == 'function') {
+		  options.after = callback;
+		}
+		
+		// serialise the props
+		var serialisedProps = [];
+		if (typeof props != string) {
+  		for (var key in props) {
+  		  if (key == 'duration') {
+  		    options.duration = props[key];
+  		  } else {
+          serialisedProps.push(key + ':' + props[key]);  		    
+  		  }
+		  }
+  		serialisedProps = serialisedProps.join(';');
+		} else {
+		  serialisedProps = props;
+		}
+		
+		return this.each(function(e){
+			emile(e, serialisedProps, options, callback);
 		});
 	}
 //---
