c3292efd231be7685f281139bf0e7dcdd17f0a05
Brian LeRoux <brian@westcoastlogic.com>
2010-04-15 14:37:49 -0700

    futzing with tween

--- src/core/fx.js
+++ src/core/fx.js
@@ -35,39 +35,39 @@ xui.extend({
 	 *
 	 * 	x$('#box').tween({ left:100px, backgroundColor:'blue' });
 	 * 	x$('#box').tween({ left:100px, backgroundColor:'blue' }, function() { alert('done!'); });
+	 
 	 * 	x$('#box').tween([{ left:100px, backgroundColor:'green', duration:.2 }, { right:'100px' }]); // WTF? is this a sequence?
 	 * 	x$('#box').tween({ left:100px}).tween({ left:'100px' });
 	 * 
 	 */
-	tween: function( props, options, callback ) {
+	// options: duration, after, easing
+	tween: function( props, callback ) {
 		// TODO make queue
-		
-		if (typeof options == 'function') {
-		  callback = options;
-		  options = {};
-		} else if (typeof options == 'number') {
-		  options = { duration: options };
-		}
+	
+		var options = {};
+		"duration after easing".split(' ').forEach( function(p) {
+    		if (props[property]) {
+    		    options[property] = props[property];
+    		    delete props[property];
+    		}
+		});
 		
 		// Here be XUI to emile options
-		
-		if (typeof callback == 'function') {
-		  options.after = callback;
-		}
+		if (typeof callback == 'function') options.after = callback;
 		
 		// serialise the props
 		var serialisedProps = [], key;
 		if (typeof props != string) {
-  		for (key in props) {
-  		  if (key == 'duration') {
-  		    options.duration = props[key];
-  		  } else {
-          serialisedProps.push(key + ':' + props[key]);  		    
-  		  }
-		  }
-  		serialisedProps = serialisedProps.join(';');
+  		    for (key in props) {
+  		        if (key == 'duration') {
+  		            options.duration = props[key];
+  		        } else {
+                    serialisedProps.push(key + ':' + props[key]);  		    
+  		        }
+		    }
+  		    serialisedProps = serialisedProps.join(';');
 		} else {
-		  serialisedProps = props;
+		    serialisedProps = props;
 		}
 		
 		return this.each(function(e){
