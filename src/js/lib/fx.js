17c20fcdc55f99f81ab92942c174a25c83e674b5
Jesse Dailey <jldailey@empi.re>
2010-05-06 20:13:38 +0000

    bug fixes: options.by and options.rotate work together, and transforms dont reset

--- src/js/lib/fx.js
+++ src/js/lib/fx.js
@@ -34,26 +34,26 @@ var Fx = {
 	 *
 	 * example:
 	 *
-	 * 	x$('#box').tween({ left:100px, backgroundColor:'blue' });
-	 * 	x$('#box').tween({ left:100px, backgroundColor:'blue' }, function() { alert('done!'); });
-	 * 	
-	 * 	x$('#box').tween([{ left:100px, backgroundColor:'green', duration:.2 }, { right:'100px' }]);
-	 * 	
-	 * 	x$('#box').tween({ left:100px}).tween({ left:'100px' });
+	 *	x$('#box').tween({ left:100px, backgroundColor:'blue' });
+	 *	x$('#box').tween({ left:100px, backgroundColor:'blue' }, function() { alert('done!'); });
+	 *	
+	 *	x$('#box').tween([{ left:100px, backgroundColor:'green', duration:.2 }, { right:'100px' }]);
+	 *	
+	 *	x$('#box').tween({ left:100px}).tween({ left:'100px' });
 	 * 
 	 */
 	tween: function( options, callback ) {
-        this.animationStack = [];	    
-	    if (options instanceof Array) {
-	        for(var i=0; i < options.length; i++) {
-	            this.animationStack.push(options[i]);
-	        }
-	    } else if (options instanceof Object) {
-	        this.animationStack.push(options);
-	    }
+		this.animationStack = [];			
+		if (options instanceof Array) {
+			for(var i=0; i < options.length; i++) {
+				this.animationStack.push(options[i]);
+			}
+		} else if (options instanceof Object) {
+			this.animationStack.push(options);
+		}
 
-	    this.start(callback);
-	    return this;
+		this.start(callback);
+		return this;
 	},
 
 	// -- private -- //
@@ -62,76 +62,81 @@ var Fx = {
 	animationStack: [],
 
 	start:function(callback) {
-	    
-	    var t = 0;
-	    var len = this.animationStack.length;
-	    
-	    for (var i = 0; i< this.animationStack.length;i++) {
+			
+		var t = 0;
+		var len = this.animationStack.length;
+			
+		for (var i = 0; i< this.animationStack.length;i++) {
 
-	        var options = this.animationStack[i];
-	        var duration     = options.duration === undefined ? 0.5    : options.duration;
-	        // We use setTimeout to stage the animations.
-	        setTimeout(function(s,o,i){
-      			s.animate(o);
-      			if ((i == len - 1) && callback && typeof(callback) == 'function') {
-      				callback();
-      			}
-      		},t*1000*duration,this,options);
-	        t += duration;
-	    }
+			var options = this.animationStack[i];
+			var duration		 = options.duration === undefined ? 0.5		 : options.duration;
+			// We use setTimeout to stage the animations.
+			setTimeout(function(s,o,i){
+				s.animate(o);
+				if ((i == len - 1) && callback && typeof(callback) == 'function') {
+					callback();
+				}
+			},t*1000*duration,this,options);
+			t += duration;
+		}
 
-	    return this;
+		return this;
 	},
-  
-	animate: function(options) {   
-	    var that = this;
-   
-	    var opt_after = options.after;
-   
-	    var easing = (options.easing === undefined) ? 'ease-in' : options.easing;
-	    var before = (options.before === undefined) ? function(){} : options.before;    
-	    var after = (opt_after === undefined) ? function(){} : function() {  opt_after.apply(that); };    
-	    var duration = (options.duration === undefined) ? 0.5 : options.duration;
-   
+	
+	animate: function(options) {	 
+		var that = this;
+	 
+		var opt_after = options.after;
+	 
+		var easing = (options.easing === undefined) ? 'ease-in' : options.easing;
+		var before = (options.before === undefined) ? function(){} : options.before;		
+		var after = (opt_after === undefined) ? function(){} : function() {  opt_after.apply(that); };		
+		var duration = (options.duration === undefined) ? 0.5 : options.duration;
+	 
 		var translate = options.by;
 		var rotate = options.rotate;
 			
-	    options.easing = options.rotate = options.by = options.before = options.after = options.duration = undefined;
-	    before.apply(before.arguments);
-   
-	    // this sets duration and easing equation on a style property change
-	    this.setStyle('-webkit-transition', 'all ' + duration + 's ' + easing );
-   
-	    // sets the starting point and ending point for each css property tween
-	    this.each( function(el) {
-	        for( var prop in options ) {
-	            that.setStyle(prop, options[prop]);
-	        }
-	
-			if (translate) {
-				that.setStyle('-webkit-transform', that.translateOp(translate[0],translate[1]));
-			}
-			
-			if (rotate) {
-				that.setStyle('-webkit-transform', that.rotateOp(rotate[0],rotate[1]));
-			}
-	    });
+		options.easing = options.rotate = options.by = options.before = options.after = options.duration = undefined;
+		before.apply(before.arguments);
+ 
+		// this sets duration and easing equation on a style property change
+		this.setStyle('-webkit-transition', 'all ' + duration + 's ' + easing );
+ 
+		// sets the starting point and ending point for each css property tween
+		this.each( function(el) {
+				for( var prop in options ) {
+						that.setStyle(prop, options[prop]);
+				}
 
-	    setTimeout(function(){ that.setStyle('-webkit-transition','none');},duration*1000);
-	    setTimeout(function(){ that.setStyle('-webkit-transform','none');},duration*1000);
-	    setTimeout(after,duration*1000);
+				var t = "";
+				if (translate) {
+					t = that.translateOp(translate[0],translate[1]));
+				}
+				if (rotate) {
+					t += " " + that.rotateOp(rotate[0],rotate[1]));
+				}
+				if( t.length > 0 ) {
+					that.setStyle('-webkit-transform', t);
+				}
+		});
 
-	    return this || that; // haha
+		setTimeout(function(){ that.setStyle('-webkit-transition','none');},duration*1000);
+		// can't reset transform or every transform just rubber-bands
+		// setTimeout(function(){ that.setStyle('-webkit-transform','none');},duration*1000);
+		setTimeout(after,duration*1000);
+
+		return this || that; // haha
 	},
 	
 	translateOp: function(xPixels, yPixels) {
-	    return 'translate(' + xPixels + 'px, ' + yPixels + 'px)';
+		return 'translate(' + xPixels + 'px, ' + yPixels + 'px)';
 	},
 	
 	rotateOp: function(axis, degree){
-	    return 'rotate' + axis.toUpperCase() + '(' + degree + 'deg)';
+		return 'rotate' + axis.toUpperCase() + '(' + degree + 'deg)';
 	}
 //---	
 };
 
 libs.push(Fx);
+
